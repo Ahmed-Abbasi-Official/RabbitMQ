3bb45c2f371c9234987f2f339fbc984157d54e61
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-08-19 11:49:52 +0500

    init

---
+++ producer.js
@@ -0,0 +1,35 @@
+const amqp = require("amqplib")
+
+const sendMail =  async() => {
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        const exchange = "mail_exchange";
+        const routingKey = "send_mail";
+
+        const message = 
+        {
+            to:"farhadkhi17@gmail.com",
+            from:"aymi.coding@gmail.com",
+            subject:"kese hu",
+            body:"heelo !!"
+        }
+
+        await channel.assertExchange(exchange,"direct",{durable:false})
+        await channel.assertQueue("mail_queue",{durable:false})
+        await channel.bindQueue("mail_queue",exchange,routingKey);
+
+        channel.publish(exchange,routingKey,Buffer.from(JSON.stringify(message)));
+
+        console.log("Mail data was sent : ",message);
+
+        setTimeout(() => {
+            connection.close();
+        }, 500);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+sendMail();
